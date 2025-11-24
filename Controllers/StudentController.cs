581161964859c05b25525a67dfea61451549d25b
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-11-24 20:06:38 +0200

    3 Controllers

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cadar_Raul_Lab1.Models;
+
+namespace Cadar_Raul_Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Info()
+        {
+            ViewBag.Message = "Mesaj din Controller";
+            ViewData["Time"] = DateTime.Now;
+
+            var student = new Student { Name = "Ana", Age = 21 };
+            return View(student);
+
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Cadar_Raul_Lab1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
