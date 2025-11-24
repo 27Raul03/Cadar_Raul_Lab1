6b5f54582efc68a33fb307712808df1b5db287e7
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2025-11-24 18:47:19 +0200

    Prima Pagina modificata

---
+++ Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cadar_Raul_Lab1.Controllers
+{
+    public class MyController : Controller
+    {
+        public string Index()
+        {
+            return "Welcome!";
+        }
+        public string Hello()
+        {
+            return "Hello World!";
+        }
+        public string More(string name, int age)
+        {
+            return $"Hello {name}, you are {age} years old!";
+        }
+    }
+}
